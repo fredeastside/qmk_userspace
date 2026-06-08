436b8497bc4eaed8db14b9e5b37f132aedc19817
Sergei Kaznakhovskii <sergei.kaznakhovskii@prizepicks.com>
2026-06-08 14:04:27 -0600

    improvements

--- keyboards/crkbd/rev1/keymaps/sergei/config.h
+++ keyboards/crkbd/rev1/keymaps/sergei/config.h
@@ -21,5 +21,3 @@
     #define RGBLIGHT_SAT_STEP 17
     #define RGBLIGHT_VAL_STEP 17
 #endif
-
-#define DYNAMIC_KEYMAP_LAYER_COUNT 5
--- keyboards/crkbd/rev1/keymaps/sergei/rules.mk
+++ keyboards/crkbd/rev1/keymaps/sergei/rules.mk
@@ -1,4 +1,7 @@
-VIA_ENABLE = yes
+# Controllers use a DFU bootloader (Elite-C / DFU Pro Micro), not promicro/caterina
+BOOTLOADER = atmel-dfu
+
+VIA_ENABLE = no
 CAPS_WORD_ENABLE = yes
 TAP_DANCE_ENABLE = yes
 LTO_ENABLE = yes
